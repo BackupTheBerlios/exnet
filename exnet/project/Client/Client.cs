73172f0b7310aa0c890efd75851cd5dabb736d2f
dhuschi <dhuschi>
2006-06-16 09:07:33 +0000

    not working yet. just playing around with remoting and basic stuff. a few classes missing

--- exnet/project/Client/Client.cs
+++ exnet/project/Client/Client.cs
@@ -13,28 +13,36 @@ using Interfaces;
 
 namespace Client
 {
-	public delegate void UserLoginDelegate(IUser user);
-	public delegate void UserSearchDelegate(List<SearchItem> hits);
-	public delegate void UserMessagesDelegate(List<Message> messages);
-	public delegate void UserInterestsDelegate(List<Interest> interests);
-	public delegate void UserWriteMessageDelegate();
-	public delegate void UserWriteInterestDelegate();
-	public delegate void UserDeleteInterestsDelegate();
-	public delegate void UserDeleteMessagesDelegate();
-	//public delegate void UserSaveFileDelegate(FileStatus fs, string FileName, object lvsi);
-	public delegate void UpdatesDelegate();	
-	
-	public class Client
+	public sealed class Client
 	{
+		#region variables
 		public event UpdatesDelegate UpdatesAvailable;
 		private string _clientStoragePath = @"client\storage\";
 		TcpChannel _chan = new TcpChannel();
-		IServer _ro;		
+		IServer _ro;	
+		public delegate void UserLoginDelegate(IUser user);
+		public delegate void UserSearchDelegate(List<SearchItem> hits);
+		public delegate void UserMessagesDelegate(List<Message> messages);
+		public delegate void UserInterestsDelegate(List<Interest> interests);
+		public delegate void UserWriteMessageDelegate();
+		public delegate void UserWriteInterestDelegate();
+		public delegate void UserDeleteInterestsDelegate();
+		public delegate void UserDeleteMessagesDelegate();
+		public delegate void UpdatesDelegate();	
+		#endregion	
 		
+		static readonly Client instance=new Client();
+		
+		#region unregioned
 		public Client()
 		{
 			this.Start();		
 		}
+
+		public static Client Instance{
+			get{
+            return instance;}
+    	}
 				
 		public void Start()
 		{			
@@ -158,142 +166,21 @@ namespace Client
 			}
 		}
 		
-		/*
-		 Calling backs
-		 */
-		
-		public delegate IUser LongRuningProcess(IUser user);
-		public delegate List<SearchItem> LongRuningSearch(string searchString, IUser user);
-		public delegate List<Message> LongRuningGetMessages(IUser user);
-		public delegate List<Interest> LongRuningGetInterests(IUser user);
-		public delegate void LongRuningWriteMessage(Message m, IUser user);
-		public delegate void LongRuningWriteInterest(Interest i, IUser user);
-		public delegate void LongRuningDeleteInterests(List <Interest> interests, IUser user);
-		public delegate void LongRuningDeleteMessages(List <Message> messages, IUser user);		
-		
-		public event UserLoginDelegate UserLogin;
-		public event UserSearchDelegate UserSearch;
-		public event UserMessagesDelegate UserMessages;
-		public event UserWriteMessageDelegate UserWriteMessage;
-		public event UserInterestsDelegate UserInterests;
-		public event UserWriteInterestDelegate UserWriteInterest;
-		public event UserDeleteInterestsDelegate UserDeleteInterests;
-		public event UserDeleteMessagesDelegate UserDeleteMessages;
-		
-#region getfile
-		//server sided interface/delegate
-		private delegate DBFile GettingFile(IUser user, DBFile dbfile);
-		
-		//filled from gui
-		public delegate void GotFileDelegate(DBFile dbfile);
-		public event GotFileDelegate GotFile;
-		
-		//gui starts getting the filestream -> async 
-		public void GetFile(IUser user, DBFile dbfile)
-		{
-			AsyncCallback call = new AsyncCallback( ServerGotFile );			
-			GettingFile process = _ro.GetFile;
-			IAsyncResult ar = process.BeginInvoke(user, dbfile, call, null);			
-		}
-		
-		//asynccallback after finishing the long running progress
-		private void ServerGotFile(IAsyncResult ar)
-		{
-			GettingFile d = (GettingFile)((AsyncResult)ar).AsyncDelegate;			
-			
-			if(GotFile != null)
-				GotFile(d.EndInvoke(ar));
-		}
-#endregion
-		
-#region filedelete
-		//server sided interface/delegate
-		private delegate DBFile DeletingFile(IUser user, DBFile dbfile);
-		
-		//filled from gui
-		public delegate void DeletedFileDelegate(DBFile dbfile);
-		public event DeletedFileDelegate FileDeleted;
-		
-		//gui starts saving the file -> async 
-		public void DeleteFile(IUser user, DBFile dbfile)
-		{
-			AsyncCallback call = new AsyncCallback( ServerFileDeleted );			
-			DeletingFile process = _ro.DeleteFile;
-			IAsyncResult ar = process.BeginInvoke(user, dbfile, call, null);			
-		}
-		
-		//asynccallback after finishing the long running progress
-		private void ServerFileDeleted(IAsyncResult ar)
-		{
-			DeletingFile d = (DeletingFile)((AsyncResult)ar).AsyncDelegate;			
-			
-			if(FileDeleted != null)
-				FileDeleted(d.EndInvoke(ar));
-		}
-#endregion
-		
-#region filesave
-		//server sided interface/delegate
-		private delegate DBFile SavingFile(FileStream fs, string clientFileHash, IUser user);
-		
-		//filled from gui
-		public delegate void SavedFileDelegate(DBFile fs);
-		public event SavedFileDelegate FileSaved;
-		
-		//gui starts saving the file -> async 
-		public void SaveFile(FileStream fs, string clientFileHash, IUser user)
-		{
-			AsyncCallback call = new AsyncCallback( ServerFileSaved );			
-			SavingFile process = _ro.SaveFile;
-			IAsyncResult ar = process.BeginInvoke(fs, clientFileHash, user, call, null);			
-		}
-		
-		//asynccallback after finishing the long running progress
-		private void ServerFileSaved(IAsyncResult ar)
-		{
-			SavingFile d = (SavingFile)((AsyncResult)ar).AsyncDelegate;			
-			
-			if(FileSaved != null)
-				FileSaved(d.EndInvoke(ar));
-		}
-#endregion
-		
-#region getfileinfos
-		//server sided interface/delegate
-		private delegate List<DBFile> GettingDBFiles(IUser user);
-		
-		//filled from gui
-		public delegate void GotDBFilesDelegate(List<DBFile> dbfiles);
-		public event GotDBFilesDelegate GotDBFiles;
-		
-		//gui starts saving the file -> async 
-		public void GetDBFiles(IUser user)
-		{
-			AsyncCallback call = new AsyncCallback( ServerGotDBFiles );			
-			GettingDBFiles process = _ro.GetFileInfos;
-			IAsyncResult ar = process.BeginInvoke(user, call, null);			
-		}
-		
-		//asynccallback after finishing the long running progress
-		private void ServerGotDBFiles(IAsyncResult ar)
-		{
-			GettingDBFiles d = (GettingDBFiles)((AsyncResult)ar).AsyncDelegate;			
-			
-			if(GotDBFiles != null)
-				GotDBFiles(d.EndInvoke(ar));
-		}
-#endregion
-
 		public void StartProcess(DBFile dbfile)
+		{
+			StartProcess(_clientStoragePath+dbfile.ID+"_"+dbfile.Name);
+		}
+		
+		public void StartProcess(string path)
 		{
 			try{
 				Process p = new Process();
-				p.StartInfo.FileName = _clientStoragePath+dbfile.ID+"_"+dbfile.FileName;
+				p.StartInfo.FileName = path;
 				p.Start();
 			}catch(Exception e){
 				Debug.WriteLine("Client: Problem starting process." + e.Message);}
 		}
-				
+		
 		public void WriteMessage(Message m, User user)
 		{
 			AsyncCallback call = new AsyncCallback( SuperWriteMessage );
@@ -352,6 +239,213 @@ namespace Client
 		
 		
 		
+		/*
+		 Calling backs
+		 */
+		
+		public delegate IUser LongRuningProcess(IUser user);
+		public delegate List<SearchItem> LongRuningSearch(string searchString, IUser user);
+		public delegate List<Message> LongRuningGetMessages(IUser user);
+		public delegate List<Interest> LongRuningGetInterests(IUser user);
+		public delegate void LongRuningWriteMessage(Message m, IUser user);
+		public delegate void LongRuningWriteInterest(Interest i, IUser user);
+		public delegate void LongRuningDeleteInterests(List <Interest> interests, IUser user);
+		public delegate void LongRuningDeleteMessages(List <Message> messages, IUser user);		
+		
+		public event UserLoginDelegate UserLogin;
+		public event UserSearchDelegate UserSearch;
+		public event UserMessagesDelegate UserMessages;
+		public event UserWriteMessageDelegate UserWriteMessage;
+		public event UserInterestsDelegate UserInterests;
+		public event UserWriteInterestDelegate UserWriteInterest;
+		public event UserDeleteInterestsDelegate UserDeleteInterests;
+		public event UserDeleteMessagesDelegate UserDeleteMessages;
+		#endregion
+	
+	#region Server Connection	
+		#region items
+			#region getitems
+			private delegate List<DBItem> GettingDBItems(IUser user, DBDirectory dir);
+			
+			public delegate void GotDBItemsDelegate(List<DBItem> dbitems);
+			public event GotDBItemsDelegate GotDBItems;
+			
+			public void GetDBItems(IUser user,DBDirectory dir)
+			{
+				AsyncCallback call = new AsyncCallback( ServerGotDBItems );			
+				GettingDBItems process = _ro.GetItems;
+				IAsyncResult ar = process.BeginInvoke(user, dir, call, null);			
+			}
+			
+			private void ServerGotDBItems(IAsyncResult ar)
+			{
+				GettingDBItems d = (GettingDBItems)((AsyncResult)ar).AsyncDelegate;			
+				
+				if(GotDBItems != null)
+					GotDBItems(d.EndInvoke(ar));
+			}
+			#endregion
+			
+		#endregion
+	
+		#region files
+			#region getfile
+			//server sided interface/delegate
+			private delegate DBFile GettingFile(IUser user, DBFile dbfile);
+			
+			//filled from gui
+			public delegate void GotFileDelegate(DBFile dbfile);
+			public event GotFileDelegate GotFile;
+			
+			//gui starts getting the filestream -> async 
+			public void GetFile(IUser user, DBFile dbfile)
+			{
+				AsyncCallback call = new AsyncCallback( ServerGotFile );			
+				GettingFile process = _ro.GetFile;
+				IAsyncResult ar = process.BeginInvoke(user, dbfile, call, null);			
+			}
+			
+			//asynccallback after finishing the long running progress
+			private void ServerGotFile(IAsyncResult ar)
+			{
+				GettingFile d = (GettingFile)((AsyncResult)ar).AsyncDelegate;			
+				
+				if(GotFile != null)
+					GotFile(d.EndInvoke(ar));
+			}
+			#endregion
+					
+			#region filedelete
+			//server sided interface/delegate
+			private delegate DBFile DeletingFile(IUser user, DBFile dbfile);
+			
+			//filled from gui
+			public delegate void DeletedFileDelegate(DBFile dbfile);
+			public event DeletedFileDelegate FileDeleted;
+			
+			//gui starts saving the file -> async 
+			public void DeleteFile(IUser user, DBFile dbfile)
+			{
+				AsyncCallback call = new AsyncCallback( ServerFileDeleted );			
+				DeletingFile process = _ro.DeleteFile;
+				IAsyncResult ar = process.BeginInvoke(user, dbfile, call, null);			
+			}
+			
+			//asynccallback after finishing the long running progress
+			private void ServerFileDeleted(IAsyncResult ar)
+			{
+				DeletingFile d = (DeletingFile)((AsyncResult)ar).AsyncDelegate;			
+				
+				if(FileDeleted != null)
+					FileDeleted(d.EndInvoke(ar));
+			}
+			#endregion
+					
+			#region filesave
+			//server sided interface/delegate
+			private delegate DBFile SavingFile(IUser user, FileStream fs, string clientFileHash, DBDirectory dir);
+
+			//filled from gui
+			public delegate void SavedFileDelegate(DBFile fs);
+			public event SavedFileDelegate FileSaved;
+			
+			//gui starts saving the file -> async 
+			public void SaveFile(IUser user, FileStream fs, string clientFileHash, DBDirectory dir)
+			{
+				AsyncCallback call = new AsyncCallback( ServerFileSaved );			
+				SavingFile process = _ro.SaveFile;
+				IAsyncResult ar = process.BeginInvoke(user, fs , clientFileHash, dir, call, null);			
+			}
+			
+			//asynccallback after finishing the long running progress
+			private void ServerFileSaved(IAsyncResult ar)
+			{
+				SavingFile d = (SavingFile)((AsyncResult)ar).AsyncDelegate;			
+				
+				if(FileSaved != null)
+					FileSaved(d.EndInvoke(ar));
+			}
+			#endregion
+					
+			#region getfiles
+			//server sided interface/delegate
+			private delegate List<DBFile> GettingDBFiles(IUser user, DBDirectory dir);
+			
+			//filled from gui
+			public delegate void GotDBFilesDelegate(List<DBFile> dbfiles);
+			public event GotDBFilesDelegate GotDBFiles;
+			
+			//gui starts saving the file -> async 
+			public void GetDBFiles(IUser user,DBDirectory dir)
+			{
+				AsyncCallback call = new AsyncCallback( ServerGotDBFiles );			
+				GettingDBFiles process = _ro.GetFiles;
+				IAsyncResult ar = process.BeginInvoke(user, dir, call, null);			
+			}
+			
+			//asynccallback after finishing the long running progress
+			private void ServerGotDBFiles(IAsyncResult ar)
+			{
+				GettingDBFiles d = (GettingDBFiles)((AsyncResult)ar).AsyncDelegate;			
+				
+				if(GotDBFiles != null)
+					GotDBFiles(d.EndInvoke(ar));
+			}
+			#endregion
+		#endregion
+		
+		
+		
+		#region directory
+			#region getdirectories
+			private delegate List<DBDirectory> GettingDBDirectories(IUser user, DBDirectory dir);
+			public delegate void GotDBDirectoriesDelegate(List<DBDirectory> dirs);
+			public event GotDBDirectoriesDelegate GotDBDirectories;
+			
+			public void GetDBDirectories(IUser user, DBDirectory dir)
+			{
+				AsyncCallback call = new AsyncCallback( ServerGotDBDirectories );			
+				GettingDBDirectories process = _ro.GetDirectoryInfos;
+				IAsyncResult ar = process.BeginInvoke(user, dir, call, null);			
+			}
+			
+			private void ServerGotDBDirectories(IAsyncResult ar)
+			{
+				GettingDBDirectories d = (GettingDBDirectories)((AsyncResult)ar).AsyncDelegate;					
+				if(GotDBDirectories != null)
+					GotDBDirectories(d.EndInvoke(ar));
+			}
+			#endregion
+		
+			#region create directory
+			private delegate DBDirectory DeletingDBDirectory(IUser user, DBDirectory dir);
+			public delegate void DeletedDBDirectoryDelegate(DBDirectory dir);
+			public event DeletedDBDirectoryDelegate DeletedDBDirectory;
+			
+			public void DeleteDBDirectory(IUser user, DBDirectory dir)
+			{
+				AsyncCallback call = new AsyncCallback( ServerDeletedDBDirectory );			
+				DeletingDBDirectory process = _ro.DeleteDirectory;
+				IAsyncResult ar = process.BeginInvoke(user, dir, call, null);			
+			}
+			
+			private void ServerDeletedDBDirectory(IAsyncResult ar)
+			{
+				DeletingDBDirectory d = (DeletingDBDirectory)((AsyncResult)ar).AsyncDelegate;			
+				if(DeletedDBDirectory != null)
+					DeletedDBDirectory(d.EndInvoke(ar));							
+			}
+			#endregion
+		#endregion
+	#endregion
+	}
+	
+	
+	public class Server
+	{
+		public string Name;
+		public int Port;
+		public string Host;
 	}
 	
 	[Serializable]	
